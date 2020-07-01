bbb33f2beef6f0fcbfe3275f5fe39a13be037b41
hagetove <67548541+hagetove@users.noreply.github.com>
2020-07-01 15:20:41 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
           Toverud Design
         </h1>
         <h2 className="title">
-          Check out <Link href="/posts/first_post"><a>the overview!</a></Link>
+          Learn <Link href="/posts/first_post"><a>more!</a></Link>
         </h2>
 
         <p className="description">
@@ -23,43 +23,20 @@ export default function Home() {
 
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>Style transfer &rarr;</h3>
+            <p>Make your own style transfer models with tensorflow.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="https://codesandbox.io/s/tfjs-examples-webcam-transfer-learning-832vv" className="card">
+            <h3>Transfer learning &rarr;</h3>
+            <p>Train a classification algorithm with TensorflowJS!</p>
           </a>
         </div>
       </main>
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          Powered by coffee
         </a>
       </footer>
 
